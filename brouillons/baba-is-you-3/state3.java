4c12ca3d9fb7da1d0f3cf9c67f7f670778ec4860
Julien Kirch <code@archiloque.net>
2019-05-13 22:28:59 +0200

    no message

--- brouillons/baba-is-you-3/state3.java
+++ brouillons/baba-is-you-3/state3.java
@@ -1,3 +1,7 @@
+/**
+ * Try to go on a direction from a position
+ * @return true if a solution has been found
+ */
 boolean tryToGo(int currentPosition, byte direction) {
   int targetPosition = calculatePosition(currentPosition, direction);
   int targetPositionContent = content[targetPosition];
@@ -22,15 +26,23 @@ boolean tryToGo(int currentPosition, byte direction) {
   }
 }
 
+/**
+ * Calculate the index of a position after a move
+ */
 private int calculatePosition(int position, byte direction) {
+  // Content is stored as a single array one line after another
   switch (direction) {
     case Direction.UP:
+      // up: go back one row
       return position - level.width;
     case Direction.DOWN:
+      // down: go further one row
       return position + level.width;
     case Direction.LEFT:
+      // left : go back one item
       return position - 1;
     case Direction.RIGHT:
+      // left : go further one item
       return position + 1;
     default:
       throw new IllegalArgumentException("" + direction);
--- brouillons/baba-is-you-4/state6.java
+++ brouillons/baba-is-you-4/state6.java
@@ -1,63 +1,68 @@
 /**
  * Process the current state
- *
- * @return the path to the solution if found, null if not.
+ * @return a list of {@link Direction} if we found a solution,
+ * else null
  */
 @Nullable byte[] processState() {
-    int babaPosition = findBaba();
-    int babaLine = babaPosition / level.width;
-    int babaColumn = babaPosition % level.width;
+  int babaPosition = findBaba();
+  int babaLine = babaPosition / level.width;
+  int babaColumn = babaPosition % level.width;
+
+  byte[] result;
+  // Up
+  if (babaLine > 0) {
+    result = tryToGo(babaPosition, Direction.UP);
+    if (result != null) {
+      return result;
+    }
+  }
+  // same for other directions
+  return null;
+}
   
-    byte[] result;
-    // Up
-    if (babaLine > 0) {
-      result = tryToGo(babaPosition, Direction.UP);
-      if (result != null) {
-        return result;
+/**
+ * Try to go on a direction from a position
+ * @return a list of {@link Direction} if we found a solution,
+ * else null
+ */
+@Nullable byte[] tryToGo(
+    int currentPosition,
+    byte direction) {
+  int targetPosition = calculatePosition(currentPosition, direction);
+  int targetPositionContent = content[targetPosition];
+
+  int[] newContent;
+  switch (targetPositionContent) {
+    case Tiles.WALL:
+      return null;
+    case Tiles.EMPTY:
+      newContent = content.clone();
+      newContent[targetPosition] = Tiles.BABA;
+      newContent[currentPosition] = Tiles.EMPTY;
+      level.addState(newContent, addMovement(direction));
+      return null;
+    case Tiles.ROCK:
+      // did we reach the border of the level ?
+      if (!canGoThere(targetPosition, direction)) {
+        return null;
       }
-    }
-    // same for other directions
-    return null;
+      // the position behind  the rock
+      int behindTheRockPosition = calculatePosition(targetPosition, direction);
+      int behindTheRockPositionContent = content[behindTheRockPosition];
+      // it it empty?
+      if (behindTheRockPositionContent != Tiles.EMPTY) {
+        return null;
+      }
+      // nice, we build the new content
+      newContent = content.clone();
+      newContent[targetPosition] = Tiles.BABA;
+      newContent[currentPosition] = Tiles.EMPTY;
+      newContent[behindTheRockPosition] = Tiles.ROCK;
+      level.addState(newContent, addMovement(direction));
+      return null;
+    case Tiles.FLAG:
+      return addMovement(direction);
+    default:
+      throw new IllegalArgumentException("" + targetPositionContent);
   }
-  
-  @Nullable byte[] tryToGo(
-      int currentPosition,
-      byte direction) {
-    int targetPosition = calculatePosition(currentPosition, direction);
-    int targetPositionContent = content[targetPosition];
-  
-    int[] newContent;
-    switch (targetPositionContent) {
-      case Tiles.WALL:
-        return null;
-      case Tiles.EMPTY:
-        newContent = content.clone();
-        newContent[targetPosition] = Tiles.BABA;
-        newContent[currentPosition] = Tiles.EMPTY;
-        level.addState(newContent, addMovement(direction));
-        return null;
-      case Tiles.ROCK:
-        // did we reach the border of the level ?
-        if (!canGoThere(targetPosition, direction)) {
-          return null;
-        }
-        // the position behind  the rock
-        int behindTheRockPosition = calculatePosition(targetPosition, direction);
-        int behindTheRockPositionContent = content[behindTheRockPosition];
-        // it it empty?
-        if (behindTheRockPositionContent != Tiles.EMPTY) {
-          return null;
-        }
-        // nice, we build the new content
-        newContent = content.clone();
-        newContent[targetPosition] = Tiles.BABA;
-        newContent[currentPosition] = Tiles.EMPTY;
-        newContent[behindTheRockPosition] = Tiles.ROCK;
-        level.addState(newContent, addMovement(direction));
-        return null;
-      case Tiles.FLAG:
-        return addMovement(direction);
-      default:
-        throw new IllegalArgumentException("" + targetPositionContent);
-    }
-  }
+}
