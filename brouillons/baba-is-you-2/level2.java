b11ed8274ccab6210a3169d090254e8514285775
Julien Kirch <code@archiloque.net>
2019-05-13 07:28:39 +0200

    no message

--- brouillons/baba-is-you-2/level2.java
+++ brouillons/baba-is-you-2/level2.java
@@ -1,19 +1,20 @@
-  final @NotNull FiFoQueue<State> states = new FiFoQueue<>();
+private final @NotNull FiFoQueue<State> states =
+    new FiFoQueue<>();
 
-  void createInitStates() {
-    addState(content);
-  }
+void createInitStates() {
+  addState(content);
+}
 
-  void addState(@NotNull int[] content) {
-    states.add(new State(this, content));
-  }
+void addState(@NotNull int[] content) {
+  states.add(new State(this, content));
+}
 
-  @Nullable State solve() {
-    while (!states.isEmpty()) {
-      State state = states.pop();
-      if (state.processState()) {
-        return state;
-      }
+@Nullable State solve() {
+  while (!states.isEmpty()) {
+    State state = states.pop();
+    if (state.processState()) {
+      return state;
     }
-    return null;
   }
+  return null;
+}
--- brouillons/baba-is-you-3/level3.java
+++ brouillons/baba-is-you-3/level3.java
@@ -3,18 +3,19 @@
  * The custom {@link Comparator} is required to avoid
  * only comparing the arrays' addresses
  */
-private final Set<int[]> pastStates = new TreeSet<>(new Comparator<>() {
-  @Override
-  public int compare(int[] o1, int[] o2) {
-    for (int i = 0; i < size; i++) {
-      int cmp = o2[i] - o1[i];
-      if (cmp != 0) {
-        return cmp;
+private final Set<int[]> pastStates =
+  new TreeSet<>(new Comparator<>() {
+    @Override
+    public int compare(int[] o1, int[] o2) {
+      for (int i = 0; i < size; i++) {
+        int cmp = o2[i] - o1[i];
+        if (cmp != 0) {
+          return cmp;
+        }
       }
+      return 0;
     }
-    return 0;
-  }
-});
+  });
 
 void addState(@NotNull int[] content) {
   if (!pastStates.contains(content)) {
