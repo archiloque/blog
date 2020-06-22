702a1f2820115787794ab420c81c64285234e6f2
Julien Kirch <code@archiloque.net>
2020-06-22 08:30:17 +0200

    no message

--- publies/baba/baba-is-you-all/generate.sh
+++ publies/baba/baba-is-you-all/generate.sh
@@ -1,6 +1,6 @@
 asciidoctor-pdf -r ./custom.rb -a pdf-fontsdir=. -a pdf-style=theme.yml README.asciidoc
-mv README.pdf baba.pdf
+mv README.pdf ../baba.pdf
 
 asciidoctor -b docbook README.asciidoc
-pandoc --from=docbook --to=epub README.xml -o baba.epub
+pandoc --from=docbook --to=epub README.xml -o ../baba.epub
 rm README.xml
