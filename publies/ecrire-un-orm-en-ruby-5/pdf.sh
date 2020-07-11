05529acaefa3111030407d7a6ba6a8cc5964a3c9
Julien Kirch <code@archiloque.net>
2020-07-12 01:00:49 +0200

    WIP

--- publies/ecrire-un-orm-en-ruby-5/pdf.sh
+++
@@ -1,7 +0,0 @@
-asciidoctor -b docbook all.asciidoc
-
-pandoc --from=docbook --to=pdf all.xml -o all.pdf --pdf-engine=xelatex -V 'monofont:Menlo.ttf' -V 'mainfont:NewYorkMedium.otf' -V 'mainfontoptions:Extension=.otf, UprightFont=*, BoldFont=*-Bold, ItalicFont=*-Italic' -V 'monofontoptions:Extension=.ttf, UprightFont=*, BoldFont=*-Bold, ItalicFont=*-Italic' -V geometry:margin=1in -V linestretch:1.25
-
-pandoc --from=docbook --to=epub all.xml -o all.epub
-
-rm all.xml
