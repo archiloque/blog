5a15a7667d1b4b6da4a81b63dde3d84a69504ea1
Julien Kirch <code@archiloque.net>
2023-04-13 22:45:46 +0200

    no message

--- publies/invisibles-networks-2023/generate-pdf.sh
+++ publies/invisibles-networks-2023/generate-pdf.sh
@@ -1,6 +1,3 @@
 asciidoctor -a stylesheet=styles.css pdf.asciidoc
 weasyprint pdf.html invisibles-networks-2023.pdf
 rm pdf.html
-asciidoctor -b docbook README.asciidoc
-pandoc -f docbook -t markdown_strict README.xml -o README.md
-rm README.xml
