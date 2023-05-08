79342a27adaecd98053c18f5b5b5f973d99ab0d9
Julien Kirch <code@archiloque.net>
2023-05-08 11:49:13 +0200

    no message

--- brouillons/wytchwood/recipes-2.rb
+++ brouillons/wytchwood/recipes-2.rb
@@ -5,8 +5,8 @@ recipes = IO.read('recipes-2.txt').split("\n").map{|i| i.split(',').map{|e| e.st
 
 File.foreach('recipes-1.txt') do |line|
     line = line.strip().split("\t")
-    sources = line[1].split(',').map{|e| e.strip}
-    sources.each do |source|
+    splitted_line = line[1].split(',').map{|e| e.strip}
+    splitted_line.each do |source|
         splitted_source = INGREDIENTS_REGEX.match(source)
         from = splitted_source[1]
         to = line[0]
@@ -22,23 +22,74 @@ def in_block()
 end    
 
 recipe_to_find = "Key to my Heart"
-main_coponents = []
+main_components = []
 
 in_block do
     STDOUT << "#{recipe_to_find} = "
     recipes.each do |recipe|
         if recipe[1] == recipe_to_find
-            main_coponents << recipe[0]
+            main_components << recipe[0]
         end
     end
-    main_coponents.sort!
-    components_formatted = main_coponents.map do |component|
-        "1 * #{component}"
+    main_components.sort!
+    components_formatted = main_components.map do |component|
+        "1 × #{component}"
     end
     STDOUT << "#{components_formatted.join(' + ')}\n"
 end
 
-main_coponents.each do |main_coponent|
+def find_recipes(recipes, component)
+    recipes.select do |recipe|
+        recipe[1] == component
+    end
+end
+
+main_components.each do |main_component|
     in_block do
+        current_components = [main_component]
+        while !current_components.empty?
+            component = current_components.shift
+            unless sources.include?(component)
+                component_recipes = find_recipes(recipes, component)
+                component_recipes.sort_by!{|recipe| recipe[0]}
+                STDOUT << "#{component} = #{component_recipes.map{|recipe| "#{recipe[2]} × #{recipe[0]}" }.join(' + ')}\n"
+                component_recipes.reverse.each do |recipe|
+                    current_components.unshift(recipe[0])
+                end
+            end
+        end
+    end
+end
+
+def components_to_s(components)
+    components.keys.sort.map{|component| "#{components[component]} × #{component.gsub(" ", " ")}" }.join(' + ')
+end
+
+in_block do
+    STDOUT << "#{recipe_to_find}\n"
+    current_recipe = {}
+    main_components.each do |component|
+        current_recipe[component] = 1
+    end
+    STDOUT << "= #{components_to_s(current_recipe)}\n"
+    has_changed = true
+    while has_changed
+        has_changed = false
+        new_recipe = Hash.new { |hash, key| hash[key] = 0 }
+        current_recipe.each_pair do |component, number|
+            if sources.include?(component)
+                new_recipe[component] += number
+            else
+                component_recipes = find_recipes(recipes, component)
+                component_recipes.each do |recipe|
+                    new_recipe[recipe[0]] += recipe[2] * number
+                end
+                has_changed = true
+            end
+        end
+        if has_changed
+            STDOUT << "= #{components_to_s(new_recipe)}\n"
+            current_recipe = new_recipe
+        end
     end
 end
