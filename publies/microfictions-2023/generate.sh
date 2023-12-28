46b7e6890b2badb97e5392a640d3cd77da154c1d
Julien Kirch <code@archiloque.net>
2023-12-28 11:44:31 +0100

    no message

--- publies/microfictions-2023/generate.sh
+++ publies/microfictions-2023/generate.sh
@@ -2,3 +2,4 @@ asciidoctor -a stylesheet=styles.css all.asciidoc
 sed -i '' 's/&#8201;&#8212;&#8201;/\&#8239;\&#8212;\&#8201;/g' all.html
 weasyprint all.html microfictions-2023.pdf
 rm all.html
+asciidoctor-epub3 all.asciidoc -a ebook-validate --out-file microfictions-2023.epub
