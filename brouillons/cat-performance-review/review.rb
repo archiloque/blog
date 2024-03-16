041bbfef7068e77a97190ffcd2ed6fb232fa7fc1
Julien Kirch <code@archiloque.net>
2024-03-16 21:42:56 +0100

    no message

---
+++ brouillons/cat-performance-review/review.rb
@@ -0,0 +1,47 @@
+require "erb"
+
+RATE_UNSATISFACTORY = "Bellow exp."
+RATE_MEETS = "Meets Exp."
+RATE_EXCEEDS = "Exceeds Exp."
+
+def standard_radio(title, group)
+    %Q(
+        <legend class="required">#{title}</legend>
+        #{radio(group, RATE_UNSATISFACTORY)}
+        #{radio(group, RATE_MEETS)}
+        #{radio(group, RATE_EXCEEDS)}
+    )
+end
+
+def radio(group, label)
+    %Q(
+        <input type="radio" name="#{group}" />
+        <label>#{label}</label>
+    )
+end
+
+def text(name, c = 'required')
+    %Q(
+    <li class="textField">
+    <label class="#{c}">#{name}</label>
+    <input type="text" />
+    </li>
+  )
+end
+
+def objective(id)
+    %Q(
+    <li class="objectiveField">
+    <label>#{id}</label>
+    <input type="text" />
+    </li>
+    <li class="objectiveRadio">
+    #{radio("objetive_#{id}", RATE_UNSATISFACTORY)}
+    #{radio("objetive_#{id}", RATE_MEETS)}
+    #{radio("objetive_#{id}", RATE_EXCEEDS)}
+</li>
+  )
+end
+
+rhtml = ERB.new(IO.read('review.html.erb'))
+File.write("review.html", rhtml.result(binding))
